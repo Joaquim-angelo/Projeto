e8b99e79df9ed214d46f8da5a9e442023b501c8e
Joaquim Angelo <156119400+Joaquim-angelo@users.noreply.github.com>
2025-08-08 16:46:39 -0300

    atualizando...

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
+const URL = "https://teachablemachine.withgoogle.com/models/SEU_MODELO/"; // <-- Cole aqui o link do seu modelo
+
 let model, webcam, labelContainer;
 
 async function init() {
-  const URL = "./model/";
   const modelURL = URL + "model.json";
   const metadataURL = URL + "metadata.json";
 
   model = await tmImage.load(modelURL, metadataURL);
-  webcam = new tmImage.Webcam(224, 224, true);
+  webcam = new tmImage.Webcam(300, 300, true);
   await webcam.setup();
   await webcam.play();
   window.requestAnimationFrame(loop);
 
   document.getElementById("webcam").appendChild(webcam.canvas);
-  labelContainer = document.getElementById("resultado");
+  labelContainer = document.getElementById("label");
 }
 
 async function loop() {
@@ -24,5 +25,7 @@ async function loop() {
 async function predict() {
   const prediction = await model.predict(webcam.canvas);
   prediction.sort((a, b) => b.probability - a.probability);
-  labelContainer.innerHTML = `Número detectado: ${prediction[0].className} (${(prediction[0].probability * 100).toFixed(2)}%)`;
+  labelContainer.innerHTML = `${prediction[0].className}: ${(prediction[0].probability * 100).toFixed(1)}%`;
 }
+
+init();
